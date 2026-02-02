3a465d49facc42c85533dfc2981bd37f308a8f58
Adham-Norqobilov <adhamnorqobilov2@gmail.com>
2026-02-02 10:35:33 +0500

    for Akmal

--- src/App.vue
+++ src/App.vue
@@ -13,7 +13,7 @@
   Lorem, ipsum dolor sit amet consectetur adipisicing elit. Enim, nihil voluptatibus laborum, ab minus aliquam, placeat mollitia eos porro necessitatibus qui tempora modi reiciendis. Unde quibusdam aliquid nesciunt quam. Vel!
   <input type="submit" value="Submit">
 </form>
-
+asdfklasdjflasdkjf lasdjf lasdfj lasdjf lasdjfla ;sjdfla;sdkjf lasdfjl ksdf jlaskdjflsad j Akmal uchun
 <div>
   <h3>Tanlangamlar</h3>
   <p>{{ javob }}</p>
